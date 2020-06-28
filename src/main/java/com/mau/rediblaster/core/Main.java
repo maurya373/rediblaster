89334b535c867406d81eb6958fbbe07f3997ba7c
maurya373 <maurya373@gmail.com>
2020-06-28 14:09:28 -0500

    use constants for benchmark and batch sizes

--- src/main/java/com/mau/rediblaster/core/Main.java
+++ src/main/java/com/mau/rediblaster/core/Main.java
@@ -11,6 +11,10 @@ import java.util.List;
 public class Main {
 
     private static long START_TIME;
+    private static final long NUM_KEYS_BENCHMARK = 10_000_000L;
+    private static final int PIPELINE_BATCH_SIZE = 1000;
+    private static final int LOG_THRESHOLD = 100_000;
+    private static final int PIPELINE_LOG_THRESHOLD = LOG_THRESHOLD / PIPELINE_BATCH_SIZE;
 
     /**
      * Benchmarking speed of 10 million SETs, GETs, and SUBSCRIBEs in redis using the java client {@link Jedis}
@@ -29,8 +33,8 @@ public class Main {
     public static void fillRedis(final Jedis jedis) {
 
         startTimer();
-        for (int i=0; i<10000000; i++) {
-            if (i % 10000 == 0)
+        for (int i=0; i<NUM_KEYS_BENCHMARK; i++) {
+            if (i > 0 && i % LOG_THRESHOLD == 0)
                 System.out.println("Finished " + i);
             jedis.set("normalKey"+i, "normalValue"+i);
         }
@@ -44,16 +48,18 @@ public class Main {
     public static void fillRedisPipelined(final Jedis jedis) {
 
         startTimer();
-        int commandBatchSize = 1000;
-        for (int i=0; i<10000; i++) {
+        long roundTrips = NUM_KEYS_BENCHMARK / PIPELINE_BATCH_SIZE;
+        for (int i=0; i<roundTrips; i++) {
 
             Pipeline pipeline = jedis.pipelined();
-            int index = i * commandBatchSize;
-            for (int j=1; j<=commandBatchSize; j++) {
+            int index = i * PIPELINE_BATCH_SIZE;
+            for (int j=1; j<=PIPELINE_BATCH_SIZE; j++) {
                 pipeline.set("pipelinedKey" + (index+j), "pipelinedValue" + (index+j));
             }
             pipeline.sync();
-            System.out.println("Completed " + i);
+            if (i > 0 && i % PIPELINE_LOG_THRESHOLD == 0) {
+                System.out.println("Completed batch " + i);
+            }
         }
         endTimer("Pipelined SETs");
     }
@@ -65,18 +71,22 @@ public class Main {
     public static void readRedisPipelined(final Jedis jedis) {
 
         startTimer();
-        int commandBatchSize = 1000;
-        for (int i=0; i<10000; i++) {
+        long roundTrips = NUM_KEYS_BENCHMARK / PIPELINE_BATCH_SIZE;
+        for (int i=0; i<roundTrips; i++) {
 
-            List<Response<String>> responseList = new ArrayList<>(commandBatchSize);
+            List<Response<String>> responseList = new ArrayList<>(PIPELINE_BATCH_SIZE);
             Pipeline pipeline = jedis.pipelined();
-            int index = i * commandBatchSize;
-            for (int j=1; j<=commandBatchSize; j++) {
+            int index = i * PIPELINE_BATCH_SIZE;
+            for (int j=1; j<=PIPELINE_BATCH_SIZE; j++) {
                 responseList.add(pipeline.get("pipelinedKey" + (index+j)));
             }
             pipeline.sync();
-            System.out.println(responseList.get(0).get());
-            System.out.println("Completed " + i);
+            if (i > 0 && i % PIPELINE_LOG_THRESHOLD == 0) {
+                // We print to verify that GETs are working AND accurate
+                // We print index+1 as the response list starts with j=1, so index+j
+                System.out.println("Latest key pipelinedKey" + (index+1) + " has value " + responseList.get(0).get());
+                System.out.println("Completed batch " + i);
+            }
         }
         endTimer("Pipelined GETs");
     }
@@ -96,18 +106,21 @@ public class Main {
 
         try {
             System.out.println("Allowing jedisPubSub to establish");
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch(Exception e) {
             System.out.println("Interruption");
         }
-        for (int i=0; i<10000000; i++) {
+
+        for (int i=0; i<NUM_KEYS_BENCHMARK; i++) {
             subscriber.subscribe("pipelinedKey"+i);
-            if (i % 10000 == 0) {
+            if (i > 0 && i % LOG_THRESHOLD == 0) {
                 System.out.println("Finished subscription " + i);
             }
         }
         endTimer("SUBSCRIBEs");
-        System.out.println("Finished subscribing to 10000000 keys");
+
+        // stop() is deprecated and unsafe, but fine for this use-case of ending the subscription thread so the
+        // benchmark process can fully terminate. Otherwise the process will stay alive.
         subscriberThread.stop();
     }
 
@@ -116,6 +129,13 @@ public class Main {
     }
 
     private static void endTimer(final String action) {
-        System.out.println(action + " took " + (System.currentTimeMillis() - START_TIME));
+        final StringBuilder builder = new StringBuilder(500);
+        builder.append(action);
+        builder.append(" took ");
+        builder.append((System.currentTimeMillis() - START_TIME));
+        builder.append(" for ");
+        builder.append(NUM_KEYS_BENCHMARK);
+        builder.append(" keys -----------------------------------------------------------------------------");
+        System.out.println(builder.toString());
     }
 }
