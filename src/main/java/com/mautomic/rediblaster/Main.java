730171f70b8ea3e00a74c858d3e09d02594782b5
mautomic <maurya373@gmail.com>
2020-07-04 13:21:57 -0500

    read and write generated Employees to redis instead of static values

--- src/main/java/com/mautomic/rediblaster/Main.java
+++ src/main/java/com/mautomic/rediblaster/Main.java
@@ -1,5 +1,6 @@
 package com.mautomic.rediblaster;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
 import redis.clients.jedis.Pipeline;
@@ -11,10 +12,11 @@ import java.util.List;
 public class Main {
 
     private static long START_TIME;
-    private static final long NUM_KEYS_BENCHMARK = 10_000_000L;
+    private static final long NUM_KEYS_BENCHMARK = 1_000_000L;
     private static final int PIPELINE_BATCH_SIZE = 1000;
     private static final int LOG_THRESHOLD = 100_000;
     private static final int PIPELINE_LOG_THRESHOLD = LOG_THRESHOLD / PIPELINE_BATCH_SIZE;
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     /**
      * Benchmarking speed of 10 million SETs, GETs, and SUBSCRIBEs in redis using the java client {@link Jedis}
@@ -24,11 +26,11 @@ public class Main {
         final Jedis jedis = new Jedis("localhost");
         fillRedisPipelined(jedis);
         readRedisPipelined(jedis);
-        subscribeRedisKeys(jedis);
+        //subscribeRedisKeys(jedis);
     }
 
     /**
-     * Fill local redis instance in non-pipelined fashion
+     * Fill local redis instance with randomly generated {@link Employee}s in non-pipelined fashion
      */
     public static void fillRedis(final Jedis jedis) {
 
@@ -36,14 +38,19 @@ public class Main {
         for (int i=0; i<NUM_KEYS_BENCHMARK; i++) {
             if (i > 0 && i % LOG_THRESHOLD == 0)
                 System.out.println("Finished " + i);
-            jedis.set("normalKey"+i, "normalValue"+i);
+            try {
+                String employeeJson = mapper.writeValueAsString(EmployeeUtil.generateEmployee(i));
+                jedis.set(String.valueOf(i), employeeJson);
+            } catch(Exception e) {
+                System.out.println("Skipped" + i + " because of error: " + e);
+            }
         }
         endTimer("Non pipelined PUTs");
     }
 
     /**
-     * Fill local redis instance in pipelined fashion. This should achieve ~5x more throughput than
-     * non-pipelined SETs
+     * Fill local redis instance with randomly generated {@link Employee}s in pipelined fashion. This should achieve
+     * ~5x more throughput than non-pipelined SETs
      */
     public static void fillRedisPipelined(final Jedis jedis) {
 
@@ -54,7 +61,13 @@ public class Main {
             Pipeline pipeline = jedis.pipelined();
             int index = i * PIPELINE_BATCH_SIZE;
             for (int j=1; j<=PIPELINE_BATCH_SIZE; j++) {
-                pipeline.set("pipelinedKey" + (index+j), "pipelinedValue" + (index+j));
+                int id = index + j;
+                try {
+                    String employeeJson = mapper.writeValueAsString(EmployeeUtil.generateEmployee(id));
+                    pipeline.set(String.valueOf(id), employeeJson);
+                } catch(Exception e) {
+                    System.out.println("Skipped" + id + " because of error: " + e);
+                }
             }
             pipeline.sync();
             if (i > 0 && i % PIPELINE_LOG_THRESHOLD == 0) {
@@ -65,7 +78,7 @@ public class Main {
     }
 
     /**
-     * Get values from local redis instance in pipelined fashion. This should achieve ~5x more throughput
+     * Get {@link Employee}s from local redis instance in pipelined fashion. This should achieve ~5x more throughput
      * than non-pipelined GETs
      */
     public static void readRedisPipelined(final Jedis jedis) {
@@ -78,13 +91,14 @@ public class Main {
             Pipeline pipeline = jedis.pipelined();
             int index = i * PIPELINE_BATCH_SIZE;
             for (int j=1; j<=PIPELINE_BATCH_SIZE; j++) {
-                responseList.add(pipeline.get("pipelinedKey" + (index+j)));
+                int id = index + j;
+                responseList.add(pipeline.get(String.valueOf(id)));
             }
             pipeline.sync();
             if (i > 0 && i % PIPELINE_LOG_THRESHOLD == 0) {
                 // We print to verify that GETs are working AND accurate
                 // We print index+1 as the response list starts with j=1, so index+j
-                System.out.println("Latest key pipelinedKey" + (index+1) + " has value " + responseList.get(0).get());
+                System.out.println("Latest key " + (index+1) + " has value " + responseList.get(0).get());
                 System.out.println("Completed batch " + i);
             }
         }
@@ -92,15 +106,16 @@ public class Main {
     }
 
     /**
-     * Subscribe to values from local redis instance. {@link JedisPubSub} does not support pipelining,
+     * Subscribe to {@link Employee}s from local redis instance. {@link JedisPubSub} does not support pipelining,
      * and consumes an entire thread as it polls redis for updates.
      */
     public static void subscribeRedisKeys(final Jedis jedis) {
 
         startTimer();
         JedisPubSub subscriber = new RedisSubscriber();
+        jedis.set("employee.meta", "value");
         Thread subscriberThread = new Thread(() -> {
-            jedis.subscribe(subscriber, "pipelinedKey1");
+            jedis.subscribe(subscriber, "employee.meta");
         }, "subscriptionThread");
         subscriberThread.start();
 
@@ -112,7 +127,7 @@ public class Main {
         }
 
         for (int i=0; i<NUM_KEYS_BENCHMARK; i++) {
-            subscriber.subscribe("pipelinedKey"+i);
+            subscriber.subscribe(String.valueOf(i));
             if (i > 0 && i % LOG_THRESHOLD == 0) {
                 System.out.println("Finished subscription " + i);
             }
